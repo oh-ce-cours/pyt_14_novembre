fd6f13dbb41290b9a5cec047cd4649ca776d9ccb
Matthieu Falce <matthieu@falce.net>
2022-11-15 11:06:46 +0100

    gitautosync update 41

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -53,6 +53,10 @@ def regle_fizzbuzz(nombre: int) -> str:
     return res
 
 
+def est_divisible(numerateur, denominateur):
+    return numerateur % denominateur
+
+
 for nombre in nombres_1_a_100:
     res = regle_fizzbuzz(nombre)
     print(res)
