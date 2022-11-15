849f69d3a5e023b030ef5f686f1c4917e515477d
Matthieu Falce <matthieu@falce.net>
2022-11-15 10:56:08 +0100

    gitautosync update 36

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -27,15 +27,15 @@ nombres_1_a_100 = range(1, 101)
 #         case _, _:
 #             print(nombre)
 
-for nombre in nombres_1_a_100:
-    res = ""
-    if nombre % 3 == 0:
-        res = res + "fizz"
-    if nombre % 5 == 0:
-        res += "buzz"
-    if not res:
-        res = nombre
-    print(res)
+# for nombre in nombres_1_a_100:
+#     res = ""
+#     if nombre % 3 == 0:
+#         res = res + "fizz"
+#     if nombre % 5 == 0:
+#         res += "buzz"
+#     if not res:
+#         res = nombre
+#     print(res)
 
 for nombre in nombres_1_a_100:
     res = "fizz" * (nombre % 3 == 0) + "buzz" * (nombre % 5 == 0) or nombre
