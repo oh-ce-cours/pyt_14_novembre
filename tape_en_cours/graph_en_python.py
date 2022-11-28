8fbe01de1f0f81e532930857340385e22c99b299
Matthieu Falce <matthieu@falce.net>
2022-11-28 16:21:22 +0100

    gitautosync update 19

--- tape_en_cours/graph_en_python.py
+++ tape_en_cours/graph_en_python.py
@@ -2,18 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import interactive
 
+interactive(True)
 xs = np.linspace(-2 * np.pi, 2 * np.pi, 100)
 ys = np.cos(xs)
 figure = plt.figure()
 axes = figure.add_subplot(111)
 axes.plot(xs, ys, "o-")
 plt.title("Super graph !")
-interactive(True)
 plt.show()
 
 figure = plt.figure()
 axes = figure.add_subplot(111)
 axes.plot(xs, ys ** 2, "o-")
 plt.title("Super graph !")
-interactive(False)
+# interactive(False)
 plt.show()
